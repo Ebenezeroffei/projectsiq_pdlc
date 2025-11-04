6750b9f30139aafb714b9bd36ae0141fc6620402
EbenezerOffei <ebenezeroffei@outlook.com>
2025-11-04 23:03:00 +0000

    🛠️: Fixed middleware error.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
 
+import { withAuth } from "next-auth/middleware"
 
-export { default } from 'next-auth/middleware'
+export default withAuth((req, _) => { },
+    {
+        callbacks: {
+            authorized: ({ token }) => !!token,
+        },
+    },
+)
 
 export const config = {
     matcher: [
